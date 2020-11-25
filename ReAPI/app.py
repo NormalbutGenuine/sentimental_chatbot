4bbbfbc5d4234368dc8099f1b7836090859d7851
NormalbutGenuine <69850657+NormalbutGenuine@users.noreply.github.com>
2020-11-25 16:57:22 +0900

    Update app.py

--- ReAPI/app.py
+++ ReAPI/app.py
@@ -1,6 +1,6 @@
 import json
 import sys
-sys.path.append('C:/Users/obybk/OneDrive/바탕 화면/AI/sentimental_chatbot/model/ET.py')
+from engineT import engine
 
 from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
@@ -22,13 +22,13 @@ def index():
     return responseBody
     
 t = 1
-while t:
-    tok = input("- ")
-    if tok != "종료":
-        answer = engine(tok)
-        print(answer)
+# while t:
+#     tok = input("- ")
+#     if tok != "종료":
+#         answer = engine(tok)
+#         print(answer)
     
-    elif tok == "종료":
-        t=0
+#     elif tok == "종료":
+#         t=0
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
